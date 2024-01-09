ea20fb9d59d0998ca799d45f58334d0a31e7d190
mishadovhiy <hi@mishadovhiy.com>
2024-01-10 00:48:36 +0200

    fonts:implemented

--- engenious-challenge/Mics/Extensions/UIFont.swift
+++ engenious-challenge/Mics/Extensions/UIFont.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 extension UIFont {
+    
     static func customFont(size:CGFloat, weight:Weight) -> UIFont {
-      //  let font:UIFont = .init(name: "SF-Pro-Text-" + weight.sfFontName, size: size)!
-        //return UIFontMetrics.default.scaledFont(for: font)
-        return .systemFont(ofSize: size, weight: weight)
+        if let font = UIFont.init(name: "SF-Pro-" + weight.sfFontName, size: size) {
+            return UIFontMetrics.default.scaledFont(for: font)
+        } else {
+            return .systemFont(ofSize: size, weight: weight)
+        }
     }
 }
 
 extension UIFont.Weight {
     var sfFontName:String {
         switch self {
-///        case .bold:return "Bold"
         default:
-            return "Regulare.otf"
+            return "Regular"
         }
     }
 }
