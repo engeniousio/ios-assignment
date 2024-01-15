b8615ea0b086d3f721111444d200d6b457d6b775
Ivan Kopych <ivan.kopych@gmail.com>
2024-01-15 10:41:21 +0200

    initial commit

--- engenious-challenge/RootViewController.swift
+++ engenious-challenge/RootViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Combine
 
+
 class RootViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let repositoryService: RepositoryService = RepositoryService()
